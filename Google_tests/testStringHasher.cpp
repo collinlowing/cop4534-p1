59f8e365c425ec97db1f2a517c61c978e0fef3d9
Collin Lowing <collin.lowing@protonmail.com>
2022-08-31 12:53:10 -0500

    implemented string hash function

--- Google_tests/testStringHasher.cpp
+++ Google_tests/testStringHasher.cpp
@@ -6,8 +6,8 @@
 
 // make hash key
 TEST(StringHasherTests, hash) {
-std::size_t expectedResult = 1746176491;
-std::string key = "password";
-std::size_t hash = StringHasher::hash(key);
+    std::size_t expectedResult = 'p'+'a'+'s'+'s'+'w'+'o'+'r'+'d';
+    std::string key = "password";
+    std::size_t hash = StringHasher::hash(key);
 EXPECT_EQ(hash, expectedResult);
 }
--- StringHasher.hpp
+++ StringHasher.hpp
@@ -5,10 +5,19 @@
 #ifndef COP4534_P1_STRINGHASHER_HPP
 #define COP4534_P1_STRINGHASHER_HPP
 
+#include <iostream>
+
 class StringHasher {
 public:
     static std::size_t hash(std::string key) {
-        return 0;
+        std::size_t total = 0;
+        for(int i = 0; i < key.length(); i++)
+        {
+            char c = key[i];
+            total += c;
+        }
+
+        return total;
     }
 };
 
